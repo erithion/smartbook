ffd210e1753e96504a792672416b9ba63c0097b3
Artyom Bondartsov <erithion@gmail.com>
2019-03-08 21:27:38 +0200

    removed gitrev

--- src/Smartbook.hs
+++ src/Smartbook.hs
@@ -1,16 +1,9 @@
-{-# LANGUAGE TemplateHaskell #-}
 module Smartbook
 ( module Internals.Crypto
 , module Internals.Book
 , module Internals.JSON
-, libraryGitInfo
 ) where
 
 import Internals.Crypto
 import Internals.Book
 import Internals.JSON
-import Development.GitRev
-
---gitLibraryInfo :: a
-libraryGitInfo = concat [ $(gitBranch), "@", $(gitHash) ]
-
